62451348480434348c5f34be055409c0cdef1524
Vijay788 <vijay788@gmail.com>
2018-02-14 11:55:24 +0530

    GitTestConsoleApp

---
+++ vj.Samples.GitConsoleApp/vj.Samples.GitConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace vj.Samples.GitConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
